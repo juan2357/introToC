ded39aedce3e6d773e39577224bf154de02efde7
juanfperez <juanfperez@me.com>
2017-09-02 10:24:10 -0400

    user asked to enter letter

--- programs/simpleInteractiveProgram/smallprogram1.c
+++ programs/simpleInteractiveProgram/smallprogram1.c
@@ -6,17 +6,24 @@
 //precompiler directives
 #define _CRT_SECURE_NO_WARNINGS //to avoid scanf warning or error
 #include <stdio.h>   //printf, scanf definitions
+#include <ctype.h>
+
 int main(void) {
 
-  double number;
-
   printf("Hello there, my name is Juan\n");
+
+  double number;
 
   printf("Please enter a number: ");
   scanf(" %lf", &number);
   printf("The product of %.0f and 3 is %.0f\n", number, (number)*3);
 
-  
+  char letter;
+
+  printf("Let's try something else...\nPlease enter a letter: ");
+  scanf(" %c", &letter);
+  printf("The letter you entered in lowercase is %c,\n", tolower(letter));
+  printf("the letter you entered in uppercase is %c\n", toupper(letter));
 
   return (0);
 }
