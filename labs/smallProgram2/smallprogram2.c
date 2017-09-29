21e44ee91979f81c5171d65d789e48be8a237d2f
juanfperez <juanfperez@me.com>
2017-09-29 12:22:12 -0400

    added header

---
+++ labs/smallProgram2/smallprogram2.c
@@ -0,0 +1,9 @@
+// Name: Juan Perez
+//Date: September 29, 2017
+//COP 2220
+/* Simple Interactive Program  2*/
+
+//precompiler directives
+#define _CRT_SECURE_NO_WARNINGS //to avoid scanf warning or error
+#include <stdio.h>   //printf, scanf definitions
+#include <ctype.h>
