ecb563a40f5fd66c5e15c5e032191caa3d52ef0a
juanfperez <juanfperez@me.com>
2017-09-29 12:23:22 -0400

    added user defined functions

--- labs/smallProgram2/smallprogram2.c
+++ labs/smallProgram2/smallprogram2.c
@@ -7,3 +7,9 @@
 #define _CRT_SECURE_NO_WARNINGS //to avoid scanf warning or error
 #include <stdio.h>   //printf, scanf definitions
 #include <ctype.h>
+//user define functions
+int GetInt(void);
+double GetDouble(void);
+char GetInitial(void);
+int FunctionOne(int x double y);
+int FunctionTwo(char x);
