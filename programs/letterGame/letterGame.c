96d033728f6506e316ad7d137af08d2f6eabf31f
juanfperez <juanfperez@me.com>
2017-10-06 19:27:28 -0400

    code complete with bugs(keeps asking for guess)

--- programs/letterGame/letterGame.c
+++ programs/letterGame/letterGame.c
@@ -40,6 +40,7 @@ int main()
 	char letter;//letter from file
   char guess;
 
+
 	//display game rules
   GameRules();
 
@@ -49,25 +50,21 @@ int main()
 	//connect to the file HINT: use fopen
   FILE * input;
   input = fopen("letterList.txt", "r");
-  fscanf(input, " %c", &letter);
-  fprintf(input, "%c\n", letter);
-
-
+  fscanf(input, "%c\n", &letter);
 	//this for loop will allow the player to play more than one game
 	//without recompiling
 	for (i = 1; i <= numGames; i++)
 	{
-    guess = GetTheGuess();
     printf("Get ready to play game %d\n", i);
 		//get a solution letter from file - use fscanf
+    fscanf(input, "%c\n", &letter);
 		//change the solution to lowercase
+    solution = tolower(letter);
 		//print the solution back onto the screen to test
-    printf("%c\n", tolower(letter));
+    printf("The letter is %c\n", tolower(letter));
 		//call the GuessTheLetter function and pass it the solution
-    // GuessTheLetter();
-
+    GuessTheLetter(guess);
 	}
-
 	//close file pointer
   fclose(input);
 	return 0;
@@ -79,31 +76,50 @@ void GameRules(){
   printf("For each game, you will have 5 chances to guess each letter\n");
   printf("Let's begin: \n");
 }
-char GetTheGuess(){
-  char c;
-  printf("Guess the letter: \n");
-  scanf(" %c\n", &c);
-  return c;
-}
-
 //this function runs one game.
 //input: character from the file, void return type
 //all other functions to Play one round of a game
 //are called from within the GuessTheLetter function
 //this function lets the user know if they have won or lost the game
-void GuessTheLetter(char solution)
-{
+void GuessTheLetter(char solution) {
 	int win = 0;
 	int numGuesses = 0;
 	//declare additional variables
-
-	while (numGuesses < MAXGUESSES && win == 0)
-	{
+	while (numGuesses < MAXGUESSES && win == 0) {
 		//get a guess from the user  by calling the GetTheGuess function
 		//change the guess to lowercase
-	    	//win = call the function to compare the guess with the solution
+    GetTheGuess();
+    //win = call the function to compare the guess with the solution
 		numGuesses++;//count the number of guesses so far
+    if (numGuesses > MAXGUESSES) {
+      printf("Sorry, you lose...\n");
+    }
 	}
-	//use conditions to let the user know if they won or lost the round of the game
 
+  //use conditions to let the user know if they won or lost the round of the game
+}
+
+char GetTheGuess(){
+  char guess;
+  char letter;
+  printf("Guess the letter: \n");
+  scanf(" %c", &guess);
+  CompareLetters(guess, letter);
+  return guess;
+}
+
+int CompareLetters(char guess, char letter){
+  if (guess == letter) {
+    printf("Congrats! You Won!\n");
+    return 1;
+  } else if (guess < letter){
+      printf("The correct answer comes after your letter\n");
+      GetTheGuess();
+
+  } else if (guess < letter) {
+      printf("The correct answer comes before your letter\n");
+      GetTheGuess();
+      
+  }
+  return 0;
 }
