24d3ae46680b7a2011ed0bddc5c259c3e2e5146e
juanfperez <juanfperez@me.com>
2017-10-15 18:40:07 -0400

    added getNum function

--- labs/smallProgram3/lab3.c
+++ labs/smallProgram3/lab3.c
@@ -7,3 +7,23 @@
 #define _CRT_SECURE_NO_WARNINGS //to avoid scanf warning or error
 #include <stdio.h>   //printf, scanf definitions
 #include <ctype.h>
+//User define functions
+//This function get a number from the user.
+int getNum(int a);
+
+
+int main() {
+  int num;
+
+  num = getNum(num);
+  printf("%d\n", num);
+
+  return 0;
+}
+//Get number from the user
+int getNum(int a){
+  int num;
+  printf("Please enter a number from 1-9: ");
+  scanf("%d", &num);
+  return num;
+}
