c5157abc019edeba521c7ba756d7e9cc34ad067f
juanfperez <juanfperez@me.com>
2017-10-15 18:57:41 -0400

    completed program

--- labs/smallProgram3/lab3.c
+++ labs/smallProgram3/lab3.c
@@ -11,20 +11,26 @@
 //This function get a number from the user.
 int getNum(int a);
 
-
-
 int main() {
   int num;
 
-
   num = getNum(num);
-  printf("%d\n", num);
 
   int i = 1;
   while (i <= num) {
     printf("%d. FAU Owls - Hoot Hoot\n", i);
     i++;
   }
+
+  for (i = 1; i <= num; i++) {
+    printf("%d. Programming is fun\n", i);
+  }
+
+  int j = 1; //needed a different variable or would cont for loop.
+  do {
+    printf("%d. Florida beaches are beautiful\n", j);
+    j++;
+  } while(j <= num);
 
 
   return 0;
