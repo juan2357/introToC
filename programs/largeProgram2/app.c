a3329e3aea826ef926a11f0e7f4ed008e68c2495
juanfperez <juanfperez@me.com>
2017-10-23 13:59:44 -0400

    setcost function working properly

--- programs/largeProgram2/app.c
+++ programs/largeProgram2/app.c
@@ -30,32 +30,68 @@ int main() {
 
   double *depositPtr;
   double deposit = 0;
-  depositPtr = &deposit;
-  char *selectionPtr;
-  char l, v, f, t, m;
+  *depositPtr = deposit;
+
+
 
 
   printf("Welcome to THE APP STORE\n\n");
 
   printf("You have $%.2f in your bank.\n\n", *depositPtr);
 
-  DisplayApps(&l);
+  char *selectionPtr;
+  char selection, l, v, f, t, m;
+  DisplayApps(&selection);
+
+  double cost;
+  double *costPtr;
+  SetCost(selection, &cost);
 
   return 0;
 }
 
 void DisplayApps(char *selectionPtr) {
-  // char l, v, f, t, l;
-  // double l = 1399.99;
-  // double v = 999.99;
-  // double f = 3.99;
-  // double t = 399.99;
-  // double m = 6.99;
+  char selection;
+
+
+
   printf("HERE ARE THE SELECTIONS:\n");
   printf("L--LogMeIn Ignition $1399.99\n");
   printf("V--VIP Black        $ 999.99\n");
   printf("F--FaceTune         $   3.99\n");
   printf("T--Tap Menu         $ 399.99\n");
-  printf("L--MineCraft        $   6.99\n");
+  printf("M--MineCraft        $   6.99\n");
+  printf("Please enter a selection: ");
+  scanf(" %c", &selection);
+  printf("you selected %c\n", selection);
+  *selectionPtr = selection;
 
 }
+
+void SetCost(char selection, double *costPtr){
+  double cost;
+  printf("%c\n", selection);
+  switch (selection) {
+    case 'l':
+      cost = 1399.99;
+      break;
+    case 'v':
+      cost = 999.99;
+      break;
+    case 'f':
+      cost = 3.99;
+      break;
+    case 't':
+      cost = 399.99;
+      break;
+    case 'm':
+      cost = 6.99;
+  }
+    printf("%.2f\n", cost);
+}
+
+// void PaymentOptions(double *depositPtr, double cost){
+//   if (deposit < cost) {
+//     printf("You do not have enough.");
+//   }
+// }
