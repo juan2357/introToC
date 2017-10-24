8273b6d6e7e7417e4e07c001cd84496dfcf51fdd
juanfperez <juanfperez@me.com>
2017-10-24 12:38:37 -0400

    added tolower function

--- programs/largeProgram2/app.c
+++ programs/largeProgram2/app.c
@@ -28,32 +28,45 @@ void DoItAgain(char *quitPtr);
 
 int main() {
 
+
   double *depositPtr;
   double deposit = 0;
   *depositPtr = deposit;
 
-
-
+  double choiceCost;
+  char quit;
+  char *quitPtr = &quit;
 
   printf("Welcome to THE APP STORE\n\n");
+  printf("You have $%.2f in your bank.\n\n", deposit);
 
-  printf("You have $%.2f in your bank.\n\n", *depositPtr);
+  do {
+    char *selectionPtr;
+    char selection, l, v, f, t, m;
+    DisplayApps(&selection);
 
-  char *selectionPtr;
-  char selection, l, v, f, t, m;
-  DisplayApps(&selection);
+    double cost;
+    double *costPtr;
+    SetCost(selection, &cost);
 
-  double cost;
-  double *costPtr;
-  SetCost(selection, &cost);
+    int compare = 0;
 
+
+    for (compare = 0; compare <= 1; compare++) {
+      PaymentOptions(&deposit, cost);
+      printf("You have $%.2f in your bank.\n\n", deposit);
+    }
+      compare = Compare(*depositPtr, choiceCost);
+      GetChange(depositPtr, cost);
+      Pay(depositPtr, choiceCost);
+      DoItAgain(quitPtr);
+
+  } while (quit == 'y');
   return 0;
 }
 
 void DisplayApps(char *selectionPtr) {
   char selection;
-
-
 
   printf("HERE ARE THE SELECTIONS:\n");
   printf("L--LogMeIn Ignition $1399.99\n");
@@ -62,15 +75,14 @@ void DisplayApps(char *selectionPtr) {
   printf("T--Tap Menu         $ 399.99\n");
   printf("M--MineCraft        $   6.99\n");
   printf("Please enter a selection: ");
-  scanf(" %c", &selection);
+  scanf(" %c", tolower(&selection));
   printf("you selected %c\n", selection);
   *selectionPtr = selection;
-
 }
 
 void SetCost(char selection, double *costPtr){
   double cost;
-  printf("%c\n", selection);
+
   switch (selection) {
     case 'l':
       cost = 1399.99;
@@ -87,11 +99,58 @@ void SetCost(char selection, double *costPtr){
     case 'm':
       cost = 6.99;
   }
-    printf("%.2f\n", cost);
+    printf("The item costs %.2f\n", cost);
 }
 
-// void PaymentOptions(double *depositPtr, double cost){
-//   if (deposit < cost) {
-//     printf("You do not have enough.");
-//   }
-// }
+void PaymentOptions(double *depositPtr, double cost){
+  int i;
+  printf("Please credit your money by selection: \n");
+  printf("--- 1 $1000.00\n");
+  printf("--- 2 $ 500.00\n");
+  printf("--- 3 $ 100.00\n");
+  printf("--- 4 $ 10.00\n");
+  scanf("%d", &i);
+  switch (i) {
+    case 1:
+      *depositPtr = *depositPtr + 1000;
+      printf("You have deposited $1000.00\n");
+      break;
+    case 2:
+      *depositPtr = *depositPtr + 500;
+      printf("You have deposited $500.00\n");
+      break;
+    case 3:
+      *depositPtr = *depositPtr + 100;
+      printf("You have deposited $100.00\n");
+      break;
+    case 4:
+      *depositPtr = *depositPtr + 10;
+      printf("You have deposited $10.00\n");
+      break;
+  }
+}
+
+int Compare(double deposit, double choiceCost){
+
+  if (deposit >= choiceCost) {
+    return 1;
+  }
+    return 0;
+}
+
+void Pay(double *depositPtr, double choiceCost){
+  *depositPtr = *depositPtr - choiceCost;
+  printf("You paid %.2f\n", choiceCost);
+}
+
+void GetChange(double *depositPtr, double choiceCost){
+  double change;
+  change = *depositPtr - choiceCost;
+  printf("You have %.2f available in you bank\n", change);
+}
+
+void DoItAgain(char *quitPtr) {
+  char quit;
+  printf("Would you like to continue (Y/N)? \n");
+  scanf("%c\n", tolower(&quit));
+}
