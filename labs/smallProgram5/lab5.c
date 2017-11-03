440913a81b82b32414d4e149731252cc9eac276a
juanfperez <juanfperez@me.com>
2017-11-03 15:40:36 -0400

    created getnum function

--- labs/smallProgram5/lab5.c
+++ labs/smallProgram5/lab5.c
@@ -3,4 +3,29 @@
 //Arrays and File Processing
 
 #define _CRT_SECURE_NO_WARNINGS
+#define SIZE 20
 #include <stdio.h>
+//get num from user
+int GetNum(int num);
+
+int main() {
+
+  int arrNum[SIZE];
+  int num;
+
+  num = GetNum();
+
+  printf("%d\n", num);
+
+  return 0;
+
+}
+
+GetNum(int num){
+
+  int num;
+
+  printf("Please enter an integer: ");
+  scanf("%d\n", &num);
+  return num;
+}
