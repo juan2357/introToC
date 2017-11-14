afb9a0e50a0ba009f83985b370eb4e06c63368ec
juanfperez <juanfperez@me.com>
2017-11-14 08:39:11 -0500

    implementedGetword function, fixed do/while loop

--- programs/largeProgram3/plural.c
+++ programs/largeProgram3/plural.c
@@ -11,21 +11,26 @@
 //Function Prototypes
 //User greeting and instructions
 void GreetUser();
+//Get word from the User
+void GetWord(char a[]);
 
 int main() {
   //declaring variables
-  char proceed;
-
+  char userChoice;
+  char word[SIZE] = " ";
   //User greeting and instructions
   GreetUser();
   //Asks user to continue
   do {
-    printf("Would you like to enter a word?\n");
-    scanf("%c", &proceed);
+    printf("Would you like to enter a word? ");
+    scanf(" %c", &userChoice);
+    if (userChoice == 'n') {
+      return 0;
+    }
+    //word from userInput
+    GetWord(word);
 
-    
-  } while(proceed == 'y');
-
+  } while (userChoice == 'y');
   return 0;
 }
 //User greeting and instructions
@@ -33,4 +38,11 @@ void GreetUser(){
   printf("Welcome to pluralizer\n");
   printf("You can enter a word \n");
   printf("and I will give you the plural...\n");
+}
+//Get word from the User
+void GetWord(char a[]){
+  printf("Enter a word: ");
+  scanf(" %s", a);
+  printf("your word is %s\n", a);
+
 }
